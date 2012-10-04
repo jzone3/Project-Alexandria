6ff5f8bd30f1e39eb342baaff9f93ccbe76699dc
Matt <matthew.lotocki@gmail.com>
2012-10-04 00:23:28 -0400

    dashboard now works

--- main.py
+++ main.py
@@ -381,22 +381,22 @@ class UploadHandler(BaseHandler):
 
 class AddBookmarkHandler(BaseHandler):
 	def get(self):
-		if self.logged_in():
-			blob_key = "AMIfv97WqLOaqvjBBJ5sqEZxsQSz_og13qyLOOfBH-7wdEj5JVnty6vCKVIlWvWOKO_1T5IgaVEHvrzejt-HxbI1Sskg48XsAdCgcPdsgSeU8sKZqTUnW-pJ3jB2JilmUfHGkuJPru1tq3-7-S77jed5pOABRzqRyw" #self.rget('blob_key')
-			guide = db.GqlQuery("SELECT * FROM Guides WHERE blob_key = '" + blob_key.replace("'", "&lsquo;") + "'").get()
-			temp_bookmark = Bookmarks(user=get_user(self.get_username()), guide=guide)
-			temp_bookmark.put()
-			
-		self.redirect('/guides')
+		self.redirect('/')
 		
 	def post(self):
 		if self.logged_in():
 			
-			blob_key = "AMIfv97WqLOaqvjBBJ5sqEZxsQSz_og13qyLOOfBH-7wdEj5JVnty6vCKVIlWvWOKO_1T5IgaVEHvrzejt-HxbI1Sskg48XsAdCgcPdsgSeU8sKZqTUnW-pJ3jB2JilmUfHGkuJPru1tq3-7-S77jed5pOABRzqRyw" #self.rget('blob_key')
+			blob_key = self.rget('id')
+			current_user = get_user(self.get_username());
+			#check to make sure user doesnt have a duplicate bookmark
+			bookmarks = Bookmarks.all();
+			bookmarks.filter('user =', current_user)
 			temp_guide = (db.GqlQuery("SELECT * FROM Guides WHERE blob_key = '" + blob_key.replace("'", "&lsquo;") + "'")).get()
-			temp_bookmark = Bookmarks(user=get_user(self.get_username()), guide=temp_guide)
-			temp_bookmark.put()
-		self.redirect('/guides')
+			bookmarks.filter('guide =', temp_guide)
+			if bookmarks.count() == 0:
+				temp_bookmark = Bookmarks(user=current_user, guide=temp_guide)
+				temp_bookmark.put()
+		#self.redirect('/guides')
 		
 class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
 	def get(self, resource):
