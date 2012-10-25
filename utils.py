c96f7b0b2c7c8c83f7fe97d1fc11d0d4c858deb9
Kenny Song <kenny.song@cohofund.com>
2012-10-25 16:22:16 -0400

    added space to title regex

--- utils.py
+++ utils.py
@@ -26,7 +26,7 @@ USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 PASS_RE = re.compile(r"^.{3,20}$")
 EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
 SCHOOL_RE= re.compile(r"^[a-zA-Z0-9 _]{1,30}$")
-TITLE_RE = re.compile(r'^[\'\.:a-zA-Z0-9_-]+$')
+TITLE_RE = re.compile(r'^[\'\.: a-zA-Z0-9_-]+$')
 PAGE_RE = r'(/(?:[\'\.:a-zA-Z0-9_-]+/?)*)'
 
 
